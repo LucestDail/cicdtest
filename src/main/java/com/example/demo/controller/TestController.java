1651761605035032159c9726bb390e872f247092
hyun <dhtmdgussksm@naver.com>
2021-07-06 18:11:55 +0900

    test

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 	
 	@GetMapping("/")
 	public String main() {
-		return "cicd test main some information change testing123123";
+		return "cicd test main some information change testing";
 	}
 }
