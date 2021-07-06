a94e1a1cbb1556c778fe6ca09225434cc1f4475e
hyun <dhtmdgussksm@naver.com>
2021-07-06 16:06:39 +0900

    testing resoucre for chekck jenkins

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 	
 	@GetMapping("/")
 	public String main() {
-		return "cicd test main some information change testing";
+		return "cicd test main some information change testing123123123";
 	}
 }
